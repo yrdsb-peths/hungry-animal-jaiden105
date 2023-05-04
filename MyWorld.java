7bf74f628357434b73ea7d44d984886e2cdfa013
jaiden105 <340867134@gapps.yrdsb.ca>
2023-05-04 09:01:40 -0400

    apple added and movement of elephant

--- MyWorld.java
+++ MyWorld.java
@@ -17,5 +17,21 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
+        createApple();
+    }
+    
+    /** 
+     * Creates a new apple at a random location at top of screen
+     * 
+     */
+    public void createApple() {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y); 
     }
 }
