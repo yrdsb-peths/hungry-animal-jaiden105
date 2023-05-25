04709cecd3a3c5bf9b892d6448f4381c822bd614
jaiden105 <340867134@gapps.yrdsb.ca>
2023-05-25 08:30:59 -0400

    added features to titlescreen

--- TitleScreen.java
+++ TitleScreen.java
@@ -18,9 +18,10 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+        addObject(titleLabel, getWidth()/2, 100);
+        prepare();
     }
-    
+
     /**
      * The main world act loop
      */
@@ -31,6 +32,22 @@ public class TitleScreen extends World
             Greenfoot.setWorld(gameWorld);
         }
     }
-    
-    
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,391,104);
+        elephant.setLocation(300,200);
+        
+        Label label = new Label("Press <space> to Start", 40);
+        addObject(label,300,350);
+        
+        Label label2 = new Label("Use <- and -> to move", 30);
+        addObject(label2,300,300);
+        
+    }
 }
